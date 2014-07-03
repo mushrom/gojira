de66baf664cfaf2fc4878ed703eda2e0ae33686e
My Name <lemrdragons@gmail.com>
2014-07-03 03:05:31 -0500

    strip_token now merges lower trees, remove_punc_tokens removes more parsing tokens

--- src/tokens.c
+++ src/tokens.c
@@ -18,13 +18,32 @@ token_t *dump_tokens( token_t *tokens, int level ){
 	return tokens;
 }
 
+// Strips a token from a tree. If the stripped token has a lower token,
+// the lower token (and all "next" nodes) will be merged into the same level
+// as the original token.
 token_t *strip_token( token_t *tokens, type_t type ){
 	token_t *ret = tokens;
+	token_t *temp;
 
 	if ( tokens ){
 		if ( tokens->type == type ){
-			ret = strip_token( tokens->next, type );
-			free( tokens );
+			if ( tokens->down ){
+				ret = strip_token( tokens->down, type );
+				temp = ret->next;
+				if ( temp ){
+					while ( temp->next ) temp = temp->next;
+					temp->next = strip_token( tokens->next, type );
+				} else {
+					ret->next = strip_token( tokens->next, type );
+				}
+
+				free( tokens );
+
+			} else {
+				ret = strip_token( tokens->next, type );
+				free( tokens );
+			}
+
 		} else {
 			ret->down = strip_token( tokens->down, type );
 			ret->next = strip_token( tokens->next, type );
@@ -42,6 +61,8 @@ token_t *remove_punc_tokens( token_t *tokens ){
 		ret = strip_token( ret, TYPE_CLOSE_PAREN );
 		ret = strip_token( ret, TYPE_APOSTR );
 		ret = strip_token( ret, TYPE_OCTOTHORPE );
+		ret = strip_token( ret, TYPE_TOKEN_LIST );
+		ret = strip_token( ret, TYPE_BASE_TOKEN );
 
 		ret->down = remove_punc_tokens( ret->down );
 		ret->next = remove_punc_tokens( ret->next );
