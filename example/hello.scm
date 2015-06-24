0a7a9c131ef7953e043e9151dc21d8d05e5967e2
My Name <nobody@itsthe.crabdance.com>
2015-06-23 23:56:49 -0400

    Removing old example program, changing hello world to be cooler

--- example/hello.scm
+++ example/hello.scm
@@ -1,30 +1,14 @@
-;(define-syntax define
-(intern-set 'define
-  (syntax-rules ()
-    ((_ sym def)
-     (intern-set 'sym def))
-    ((_ sym)
-     (intern-set 'sym 0))))
+#!/usr/bin/env gojira
 
-(define print
-  (lambda (a)
-    (display a)
-    (newline)))
+(import! 'lists)
 
-(define meh
-  (lambda (test)
-    ;(intern-set 'bar 'bar)
-    (define bar 'bar)
-    (lambda (foo)
-      (display test)
-      (display bar)
-      (display foo)
-      (newline)))) 
+(define make-hello
+  (lambda (str)
+    (lambda ()
+      (define str (string-append "hello, " str))
+      str)))
 
-(display "Hello, world!")
-(newline)
+(define world (make-hello "world!"))
 
-(display "1 + 2 is ")
-;(display (+ 1 2)) (newline)
-(print (+ 1 2))
-(print ((meh "asdf") ':wut))
+(foreach (iota 5)
+  (lambda (n) (print (world))))
