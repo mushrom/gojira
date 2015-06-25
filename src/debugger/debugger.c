23142521b2a734113b7ef0705ebadbffc81e23da
My Name <nobody@itsthe.crabdance.com>
2015-06-25 03:30:39 -0500

    Adding heap debugging function

--- src/debugger/debugger.c
+++ src/debugger/debugger.c
@@ -1,9 +1,47 @@
 #include <gojira/config.h>
 #include <gojira/debugger/debugger.h>
 #include <gojira/runtime/runtime.h>
+#include <gojira/runtime/garbage.h>
 #include <linenoise/linenoise.h>
 #include <stdio.h>
 #include <string.h>
+
+static unsigned p_heap( token_t *heap ){
+	token_t *move = heap;
+	unsigned i = 0;
+	unsigned unmarked = 0;
+	unsigned marked = 0;
+	unsigned freed = 0;
+
+	for ( ; move; move = move->gc_link ){
+		switch ( move->status ){
+			case GC_UNMARKED: unmarked++; break;
+			case GC_MARKED:   marked++; break;
+			case GC_FREED:    freed++; break;
+			default: break;
+		};
+
+		i++;
+	}
+
+	printf( "%3u tokens, %3u unmarked, %3u marked, %3u freed",
+			i, unmarked, marked, freed );
+
+	return i;
+}
+
+static unsigned dump_frame_heaps( stack_frame_t *frame, unsigned limit, unsigned n ){
+	unsigned total = 0;
+
+	if ((( limit && n < limit ) || !limit ) && frame ){
+		printf( "frame %3u: ", n );
+		total += p_heap( frame->heap );
+		printf( "\n" );
+		total += dump_frame_heaps( frame->last, limit, n + 1 );
+	}
+
+	return total;
+}
 
 token_t *debugger_loop( stack_frame_t *frame ){
 	char *buf;
@@ -15,7 +53,7 @@ token_t *debugger_loop( stack_frame_t *frame ){
 		buf = linenoise( "debug > " );
 
 		if ( buf ){
-			if ( strcmp( buf, "exit" ) == 0 ){
+			if ( strcmp( buf, "exit" ) == 0 || strcmp( buf, "cont" ) == 0 ){
 				printf( "[%s] All done\n", __func__ );
 				running = false;
 				break;
@@ -25,9 +63,11 @@ token_t *debugger_loop( stack_frame_t *frame ){
 
 			} else if ( strcmp( buf, "help" ) == 0 ){
 				printf( "%8s | show this help\n", "help" );
+				printf( "%8s | show a trace of each frame's heap\n", "heap" );
+				printf( "%8s | show the interpreter's current status\n", "status" );
 
-			} else if ( strcmp( buf, "step" ) == 0 ){
-				eval_step( &frame );
+			} else if ( strcmp( buf, "heap" ) == 0 ){
+				printf( "%3u tokens total.\n", dump_frame_heaps( frame, 10000, 0 ));
 
 			} else {
 				printf( "[%s] Unknown command.", __func__ );
